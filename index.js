de51b0533479e762bc5bb55157efba3abc86c393
Jungin <jungin.dylan@gmail.com>
2024-12-05 11:55:33 +0900

    test

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express() 
 const port = 3000
 app.get('/', (req, res) => { 
-  res.send('Hello World Testing!') 
+  res.send('Hello World Testing CICD!') 
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening`)
 })
