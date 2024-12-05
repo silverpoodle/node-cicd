ca4a4c79b474785429dbb01a86c0aef459c61455
Jungin <jungin.dylan@gmail.com>
2024-12-05 12:17:05 +0900

    test

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express() 
-const port = 5000
-app.get('/', (req, res) => { 
-  res.send('Hello World Testing CICD!') 
-})
+// app.js
+const http = require('http');
 
-app.listen(port, () => {
-  console.log(`Example app listening`)
-})
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Node JS Server is  running at http://${hostname}:${port}/`);
+});
+
+module.exports = server;
